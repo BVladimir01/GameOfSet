5c98c743ebb2b21a0dee88916f1fe6fe4ede5cbe
Vladimir <bizin.vladimir01@gmail.com>
2024-09-25 09:39:27 +0300

    Initial Commit

---
+++ GameOfSet/GameOfSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfSetApp.swift
+//  GameOfSet
+//
+//  Created by Vladimir on 25.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct GameOfSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
